d856a1eddb46e1f6869b6633040615d54afd6028
Daniel <daniel.neubauer@pmone.com>
2016-07-30 20:53:34 +0200

    feat(test): add table model to test project

---
+++ Engine.Test/Models/User.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Engine.Test.Models
+{
+    class User
+    {
+        [Column(PrimaryKey = true, AutoIncrement = true, Datatype = "INTEGER", Name = "Id")]
+        public int Id { get; set; }
+        [Column(Datatype = "NVARCHAR", Length = 250, Name = "Name")]
+        public string Name { get; set; }
+        [Column(Datatype = "BOOLEAN", Length = 250, Name = "Used")]
+        public bool InUse { get; set; }
+    }
+
+}
+}
