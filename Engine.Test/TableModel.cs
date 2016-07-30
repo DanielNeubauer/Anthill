f67b0a96f4b404c350a98ca1a67ee65ea1cabb78
Daniel <daniel.neubauer@pmone.com>
2016-07-30 20:49:27 +0200

    feat(test): add xunit test project

---
+++ Engine.Test/TableModel.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Linq;
+using Xunit;
+
+namespace Engine.Test
+{
+    public class UnitTest1
+    {
+        [Fact]
+
+        public void TestTableModelGeneration()
+        {
+        }
+    }
+}
