b827490c3d02ab1bd3fb6e802495365ff98237cf
Opiskull <Christian.Onuk@gmx.net>
2016-07-31 18:44:34 +0200

    feat(sql-query-builder): add initial implementation of sqlquerybuilder

---
+++ src/Core/Services/SqlQueryBuilder.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Anthill.Engine.Services
+{
+    public class SqlQueryBuilder
+    {
+        public SqlQueryBuilder()
+        {
+
+        }
+
+        private StringBuilder stringBuilder = new StringBuilder();
+
+        public SqlQueryBuilder Select(params string[] parameters)
+        {
+            stringBuilder.Append("SELECT ");
+            stringBuilder.Append(string.Join(",", parameters));
+            return this;
+        }
+
+        public SqlQueryBuilder From(string tableName)
+        {
+            stringBuilder.Append(" FROM {tableName}");
+            return this;
+        }
+
+        public SqlQueryBuilder Where(string[] whereClauses)
+        {
+            stringBuilder.Append(" WHERE ");
+            stringBuilder.Append(string.Join(" AND ", whereClauses));
+            return this;
+        }
+
+        public SqlQueryBuilder OrderBy(params string[] fields)
+        {
+            stringBuilder.Append(" ORDER BY ");
+            stringBuilder.Append(string.Join(",", fields));
+            return this;
+        }
+
+        public string ToQuery()
+        {
+            return stringBuilder.ToString();
+        }
+    }
+}
