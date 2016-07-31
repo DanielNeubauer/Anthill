5ca316fc4611057bbb7947420f2b8c1fe2849cc2
Opiskull <Christian.Onuk@gmx.net>
2016-07-31 18:55:21 +0200

    test(sql-query-builder): add test for sqlQueryBuilder

---
+++ src/Core.Test/SqlQueryBuilderTests.cs
@@ -0,0 +1,22 @@
+﻿using Anthill.Engine.Services;
+using Xunit;
+
+namespace Anthill.Core.Test
+{
+    public class SqlQueryBuilderTests
+    {
+        [Fact]
+        public void TestFullSelect()
+        {
+            var queryBuilder = new SqlQueryBuilder();
+
+            var query = queryBuilder
+                .Select("Id", "Name", "InUse")
+                .From("User")
+                .Where("Name = 'hamster'")
+                .ToQuery();
+
+            Assert.Equal("SELECT Id, Name, InUse FROM User WHERE Name = 'hamster'", query);
+        }
+    }
+}
